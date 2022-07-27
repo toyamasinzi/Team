4b83f328c29cf6a0492857c25955d253c96f73dd
sinzitoyama <yamat422@gmail.com>
2022-07-28 01:26:38 +0900

    エネミー過程

--- Assets/MyScript/Enemy/Enemy.cs
+++ Assets/MyScript/Enemy/Enemy.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour,IDamegable
+public class Enemy : MonoBehaviour, IDamegable
 {
     [SerializeField] GameObject _player1;//オブジェクトを参照する
-   // [SerializeField] GameObject _player2;
+                                         // [SerializeField] GameObject _player2;
     [SerializeField] float _speed = 1f;
     [SerializeField] int _enemyHP = 3;
     [SerializeField] int _attackPower = 1;
@@ -19,50 +19,45 @@ public class Enemy : MonoBehaviour,IDamegable
     }
     private void Update()
     {
-        if (_plt)
-        {
-            _speed = 1f;
-            Vector2 targeting = (_player1.transform.position - transform.position).normalized;//プレイヤー-敵キャラの位置関係から方向を取得し、速度を一定化
-            GetComponent<Rigidbody2D>().velocity = new Vector2(targeting.x * _speed, 0);//プレイヤー追う
-            if (targeting.x > 0)
-            {
-                transform.rotation = new Quaternion(0, 1, 0, 0);
-                _anim.SetFloat("Move", 1f);
-            }
-            else
-            {
-                transform.rotation = new Quaternion(0, 0, 0, 0);
-                _anim.SetFloat("Move", 1f);
-            }
-        }
         if (_plt == false)
         {
             _anim.SetFloat("Move", 0.1f);
-            _speed = 0f;
         }
     }
-            private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player"|| collision.gameObject.tag == "Player2" )
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Player2")
         {
-            Debug.Log("1");
-            _plt = true;
+            _plt = true;    
+                Vector2 targeting = (_player1.transform.position - transform.position).normalized;//プレイヤー-敵キャラの位置関係から方向を取得し、速度を一定化
+                GetComponent<Rigidbody2D>().velocity = new Vector2(targeting.x * _speed, 0);//プレイヤー追う
+                if (targeting.x > 0)
+                {
+                    transform.rotation = new Quaternion(0, 1, 0, 0);
+                    _anim.SetFloat("Move", 1f);
+                }
+                else
+                {
+                    transform.rotation = new Quaternion(0, 0, 0, 0);
+                    _anim.SetFloat("Move", 1f);
+                }
         }
+        
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Player2")
         {
-            Debug.Log("2");
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
             _plt = false;
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
             IDamegable player = collision.gameObject.GetComponent<IDamegable>();
-            if(player != null)
+            if (player != null)
             {
                 player.Damage(_attackPower);
             }
@@ -76,5 +71,4 @@ public class Enemy : MonoBehaviour,IDamegable
             this.gameObject.SetActive(false);
         }
     }
-
 }
